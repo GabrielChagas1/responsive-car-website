25fc1a1757582e38dc154e9e33d9df66d2148c5f
Gabriel Chagas <gabrielserq@outlook.com>
2022-04-30 15:17:32 -0300

    chore: retrieving menu classes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
