1d21eec0a509d3a487efe2c24718ccebe0a10b27
Gabriel Chagas <gabrielserq@outlook.com>
2022-05-13 09:47:48 -0300

    chore: js scroll reveal.

--- assets/js/main.js
+++ assets/js/main.js
@@ -106,3 +106,23 @@ function scrollActive(){
 window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  // reset: true
+})
+
+sr.reveal(`.home__title, .popular__container, .features__img, .featured__filters`)
+sr.reveal(`.home__subtitle`, {delay: 500})
+sr.reveal(`.home__elec`, {delay: 600})
+sr.reveal(`.home__img`, {delay: 800})
+sr.reveal(`.home__car-data`, {delay: 900, interval: 100, origin: 'bottom'})
+sr.reveal(`.home__button`, {delay: 1000, origin: 'bottom'})
+sr.reveal(`.about__group`, {origin: 'left'})
+sr.reveal(`.about__data`, {origin: 'right'})
+sr.reveal(`.features__map`, {delay: 600, origin: 'right'})
+sr.reveal(`.features__card`, {interval: 300})
+sr.reveal(`.featured__card, .logos__container, .footer__content`, {interval: 100})
+
